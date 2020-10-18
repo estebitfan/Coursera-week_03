e8845bd0d8bfe7294d4cff825196c1cb55b42377
egonzalez_improgress <egonzalez@improgress.net>
2020-10-18 18:05:40 -0500

    First commit to GIT using this repository.
    Coursera week 03 assignment.

---
+++ Code/ProgrammingAssignment2.R
@@ -0,0 +1,39 @@
+## A pair of functions that cache the inverse of a matrix.
+## This function creates a special "matrix" object that can cache its inverse.  
+makeCacheMatrix <- function(x = matrix()) {
+          inverse <- NULL
+          set <- function(y){
+                    # <<- sets the variable outside the scope of the function
+                    x <<- y
+                    inverse <<- NULL
+          }
+
+          get <- function(){ x }
+          setInverse <- function(solveMatrix){ inverse <<- solveMatrix }
+          getInverse <- function() { inverse }
+          
+          #List of functions to set, get, setInverse and getInverse
+          #From the special matrix
+          list(set = set,
+               get = get,
+               setInverse = setInverse,
+               getInverse = getInverse)
+}
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## Checks if the mean was calculated before, if not: it calculates the mean.
+cacheSolve <- function(x, ...) {
+          
+          ## Return a matrix that is the inverse of 'x'
+          inv <- x$getInverse()
+          if(!is.null(inv)){
+                   #Returns cached data
+                    return(inv)
+          }
+          data <- x$get()
+          inv <- solve(data)
+          x$setInverse(inv)
+          inv      
+}
+
+
